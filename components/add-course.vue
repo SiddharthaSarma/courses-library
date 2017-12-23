3d80e7a6749fb87b5a7e7f0b95384837f0499c07
Siddhartha <sidduinevitable@gmail.com>
2017-12-23 10:53:41 +0530

    Added functionality to get the form values.

--- components/add-course.vue
+++ components/add-course.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="row">
-    <form class="col s12">
+    <form class="col s12" @submit.prevent="submitForm">
       <div class="row">
         <div class="input-field col s6">
-          <input id="courseName" type="text" class="validate">
+          <input id="courseName" type="text" v-model="courseName" class="validate">
           <label for="courseName">Course Name</label>
         </div>
         <div class="input-field col s6">
-          <select name="courseLanguage" id="ddlLanguage">
+          <select name="courseLanguage" id="ddlLanguage" v-model="courseLanguage">
             <option v-for="opt in languages" :value="opt.id" :key="opt.id">{{opt.value.name}}</option>
           </select>
           <label for="txtLanguage">Language</label>
@@ -15,50 +15,58 @@
       </div>
       <div class="row">
         <div class="input-field col s12">
-          <input id="txtAutorName" type="text" class="validate">
+          <input id="txtAutorName" type="text" class="validate" v-model="courseAuthor">
           <label for="txtAutorName">Author Name</label>
         </div>
       </div>
       <div class="row">
         <div class="input-field col s12">
-          <input id="txtCourseLink" type="text" class="validate">
+          <input id="txtCourseLink" type="text" class="validate" v-model="courseLink">
           <label for="txtCourseLink">Course Link</label>
         </div>
       </div>
       <div class="row">
         <div class="input-field col s12">
-          <input id="txtImageLink" type="text" class="validate">
+          <input id="txtImageLink" type="text" class="validate" v-model="imageLink">
           <label for="txtImageLink">Image Link</label>
         </div>
       </div>
       <div class="row">
         <div class="input-field col s6">
-          <select name="coursePublisher" id="ddlPublisher">
+          <select name="coursePublisher" id="ddlPublisher" v-model="coursePublisher">
             <option v-for="opt in publishers" :value="opt.id" :key="opt.id">{{opt.value.name}}</option>
           </select>
           <label for="coursePublisher">Publisher</label>
         </div>
         <div class="input-field col s6">
-          <input id="txtPublishedDate" type="text">
+          <input id="txtPublishedDate" type="text" v-model="publishedDate">
           <label for="txtPublishedDate">Published Date</label>
         </div>
       </div>
+       <button class="btn waves-effect waves-light" type="submit" name="action">Create
+        <i class="material-icons right">send</i>
+      </button>
     </form>
   </div>
 </template>
 
 <script>
+
 import Vue from 'vue';
 import { Component } from 'vue-property-decorator';
 import db from '../firebaseinit';
+
 @Component({})
 export default class AddCourse extends Vue {
   languages = [];
   publishers = [];
+  courseName = '';
+
   created() {
     this.fetchLanguages();
     this.fetchPublishers();
   }
+
   fetchLanguages() {
     db
       .collection('languages')
@@ -75,6 +83,11 @@ export default class AddCourse extends Vue {
         });
       });
   }
+  // Need to save details in firebase.
+  submitForm() {
+    console.log(this.courseName, this.courseAuthor, this.courseLink, this.imageLink, this.coursePublisher, this.publishedDate);
+  }
+
   fetchPublishers() {
     db
       .collection('publisher')
@@ -91,6 +104,7 @@ export default class AddCourse extends Vue {
         });
       });
   }
+  
   mounted() {
     $('#txtPublishedDate').pickadate({
       selectMonths: true, // Creates a dropdown to control month

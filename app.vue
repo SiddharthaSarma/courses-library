c3ef8bae91dfcfed452bd3bec31cbdb51d932bd5
Siddhartha <sidduinevitable@gmail.com>
2017-12-25 19:14:52 +0530

    Changed colors for the app

--- app.vue
+++ app.vue
@@ -1,25 +1,25 @@
 <template>
-  <v-app dark>
-		<v-toolbar class="teal darken-3">
-			<v-toolbar-title>Title</v-toolbar-title>
-			<v-spacer></v-spacer>
-			<v-toolbar-side-icon class="hidden-md-and-up"></v-toolbar-side-icon>
-			<v-toolbar-items class="hidden-sm-and-down">
-				<v-btn flat @click="toggleList(true)">List</v-btn>
-				<v-btn flat @click="toggleList(false)">create course</v-btn>
-			</v-toolbar-items>
-  	</v-toolbar>
-		<v-content>
-			<v-layout>
-				<v-flex v-show="showList">
-					<List />
-				</v-flex>
-				<v-flex class="pa-3" v-if="!showList">
-					<vform></vform>
-				</v-flex>
-		  </v-layout>
-	  </v-content>
-	</v-app>
+  <v-app light>
+    <v-toolbar dark class="primary">
+      <v-toolbar-title>Courses library</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-side-icon class="hidden-md-and-up"></v-toolbar-side-icon>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn flat @click="toggleList(true)">List</v-btn>
+        <v-btn flat @click="toggleList(false)">create course</v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+    <v-content>
+      <v-layout>
+        <v-flex v-if="showList">
+          <List />
+        </v-flex>
+        <v-flex class="pa-3" v-if="!showList">
+          <vform></vform>
+        </v-flex>
+      </v-layout>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
@@ -28,7 +28,7 @@ import db from './firebaseinit';
 import List from './components/list';
 import vform from './components/add-course';
 import { Component } from 'vue-property-decorator';
-import('./node_modules/vuetify/dist/vuetify.min.css')
+import('./node_modules/vuetify/dist/vuetify.min.css');
 @Component({
   components: {
     List,
@@ -42,7 +42,7 @@ export default class App extends Vue {
     db
       .collection('languages')
       .add({
-        name: 'C',
+        name: 'C'
       })
       .then(function(docRef) {
         console.log('Document written with ID: ', docRef.id);

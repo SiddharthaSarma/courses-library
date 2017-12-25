fff1680a27c90e1006ec168a61aa168142d808c8
Siddhartha <sidduinevitable@gmail.com>
2017-12-25 19:27:37 +0530

    Updated colors for the app.

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ import Vue from 'vue';
 import App from './app.vue';
 import Vuetify from 'vuetify';
 
-Vue.use(Vuetify);
+Vue.use(Vuetify, {
+  theme: {
+    primary: '#d32f2f',
+    secondary: '#bdbdbd',
+    accent: '#ff5252',
+    error: '#d50000'
+  }
+});
 new Vue({
   el: '#app',
   render: h => h(App)

76ac62ba763626bb93d3c4199e629208aef3cba2
Siddhartha <sidduinevitable@gmail.com>
2017-12-26 15:57:58 +0530

    Added functionality to show the loading on page load

--- components/list.vue
+++ components/list.vue
@@ -1,6 +1,7 @@
 <template>
   <v-container fluid grid-list-md>
     <v-layout row wrap>
+      <div class="spinner" v-if="loading"></div>
       <v-flex xs12 sm6 md3 v-for="course in list" :key="course.id">
         <v-card flat tile>
           <v-card-media :src="course.imageLink" height="150px">
@@ -27,15 +28,18 @@
 import Vue from 'vue';
 import { Component } from 'vue-property-decorator';
 import db from '../firebaseinit';
+
 @Component({})
 export default class List extends Vue {
   list = [];
+  loading = true;
   created() {
     db
       .collection('courses')
       .get()
-      .then((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
+      .then(querySnapshot => {
+        this.loading = false;
+        querySnapshot.forEach(doc => {
           this.list.push(doc.data());
         });
       });
@@ -47,5 +51,16 @@ export default class List extends Vue {
 .course-description {
   max-height: 150px;
   overflow: hidden;
+}
+.spinner {
+  background-image: url(../assets/preloader.gif);
+  background-position: center;
+  background-repeat: no-repeat;
+  height: 200px;
+  left: 50%;
+  margin: -100px 0 0 -100px;
+  position: absolute;
+  top: 50%;
+  width: 200px;
 }
 </style>

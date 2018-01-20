2d00d214cfa72b9eeed61d6b61383b4e54854bca
siddharthasarma <sidduinevitable@gmail.com>
2018-01-20 18:35:57 +0530

    Using event bus for the search

--- src/components/search.vue
+++ src/components/search.vue
@@ -4,10 +4,11 @@
     <v-flex>
       <div class="mb-3 input-group input-group--prepend-icon input-group--text-field input-group--single-line input-group--solo primary--text">
         <label>Search</label>
-        <div class="input-group__input"><i aria-hidden="true" class="material-icons icon input-group__prepend-icon">filter_list</i>
-          <input tabindex="0" placeholder="Search" type="text"><i aria-hidden="true" class="material-icons icon input-group__append-icon"></i></div>
-        <div class="input-group__details">
-        </div>
+        <div class="input-group__input">
+          <i aria-hidden="true" class="material-icons icon input-group__prepend-icon">filter_list</i>
+          <input tabindex="0" placeholder="Search" type="text" v-model="search" v-on:change="searchCourse">
+            <i aria-hidden="true" class="material-icons icon input-group__append-icon"></i>
+          </div>
       </div>
     </v-flex>
   </v-layout>
@@ -16,7 +17,18 @@
 </template>
 
 <script>
-export default {};
+import Vue from 'vue';
+import EventBus from '../eventbus.js';
+import { Component } from 'vue-property-decorator';
+
+@Component({})
+export default class Search extends Vue {
+  search = 'Hi there';
+
+  searchCourse() {
+    EventBus.$emit('SEARCH_COURSE', { search: this.search });
+  }
+}
 </script>
 
 <style>

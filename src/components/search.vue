9eb5c239a9160782a0cb95cb34659cc9dae8facd
siddharthasarma <sidduinevitable@gmail.com>
2018-01-20 18:54:50 +0530

    Formatted the code

--- src/components/search.vue
+++ src/components/search.vue
@@ -1,19 +1,18 @@
 <template>
-    <v-content>
-  <v-layout>
-    <v-flex>
-      <div class="mb-3 input-group input-group--prepend-icon input-group--text-field input-group--single-line input-group--solo primary--text">
-        <label>Search</label>
-        <div class="input-group__input">
-          <i aria-hidden="true" class="material-icons icon input-group__prepend-icon">filter_list</i>
-          <input tabindex="0" placeholder="Search" type="text" v-model="search" v-on:change="searchCourse">
-            <i aria-hidden="true" class="material-icons icon input-group__append-icon"></i>
-          </div>
-      </div>
-    </v-flex>
-  </v-layout>
-</v-content>
-
+  <v-content>
+    <v-layout>
+      <v-flex>
+        <div class="mb-3 input-group input-group--prepend-icon input-group--text-field input-group--single-line input-group--solo primary--text">
+          <label>Search</label>
+          <div class="input-group__input">
+            <i aria-hidden="true" class="material-icons icon input-group__prepend-icon">filter_list</i>
+            <input tabindex="0" placeholder="Search" type="text" v-model="search" v-on:change="searchCourse">
+              <i aria-hidden="true" class="material-icons icon input-group__append-icon"></i>
+            </div>
+        </div>
+      </v-flex>
+    </v-layout>
+  </v-content>
 </template>
 
 <script>

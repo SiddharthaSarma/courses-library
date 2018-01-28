48b6571a3714b20323f0581272347f62f5950227
siddharthasarma <sidduinevitable@gmail.com>
2018-01-28 20:28:33 +0530

    changed onchange method to keyup method

--- src/components/search.vue
+++ src/components/search.vue
@@ -5,7 +5,7 @@
         <div class="mb-3 input-group input-group--prepend-icon input-group--text-field input-group--single-line input-group--solo primary--text">
           <div class="input-group__input">
             <i aria-hidden="true" class="material-icons icon input-group__prepend-icon">search</i>
-            <input tabindex="0" placeholder="Search" type="text" v-model="search" v-on:change="searchCourse">
+            <input tabindex="0" placeholder="Search" type="text" v-model="search" v-on:keyup="searchCourse">
               <i aria-hidden="true" class="material-icons icon input-group__append-icon"></i>
             </div>
         </div>

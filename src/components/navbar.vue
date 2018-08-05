f5a664f10c0a9e2ffb9e3e271e8ba5e388b260bc
siddharthasarma <sidduinevitable@gmail.com>
2018-08-05 19:47:47 +0530

    Created navbar component

---
+++ src/components/navbar.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-toolbar class="primary">
+    <v-toolbar-title>Courses library</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-toolbar-side-icon class="hidden-md-and-up"></v-toolbar-side-icon>
+    <v-toolbar-items class="hidden-sm-and-down">
+      <v-btn flat @click="navigateTo('library')">List</v-btn>
+      <v-btn flat @click="navigateTo('add')">create course</v-btn>
+    </v-toolbar-items>
+  </v-toolbar>
+</template>
+
+<script>
+import Vue from 'vue';
+import { Component } from 'vue-property-decorator';
+@Component({})
+export default class Navbar extends Vue {
+  navigateTo(path) {
+    this.$router.push(path);
+  }
+}
+</script>

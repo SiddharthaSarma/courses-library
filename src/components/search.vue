1672771453b1f98efb6c5a49fba36ab44011c952
siddharthasarma <sidduinevitable@gmail.com>
2019-03-19 21:23:14 +0530

    format the code

--- src/components/search.vue
+++ src/components/search.vue
@@ -3,12 +3,20 @@
     <v-layout>
       <v-flex xs8></v-flex>
       <v-flex xs4>
-        <div class="mb-3 input-group input-group--prepend-icon input-group--text-field input-group--single-line input-group--solo">
+        <div
+          class="mb-3 input-group input-group--prepend-icon input-group--text-field input-group--single-line input-group--solo"
+        >
           <div class="input-group__input">
             <i aria-hidden="true" class="material-icons icon input-group__prepend-icon">search</i>
-            <input tabindex="0" placeholder="Search" type="text" v-model="search" v-on:keyup="searchCourse">
-              <i aria-hidden="true" class="material-icons icon input-group__append-icon"></i>
-            </div>
+            <input
+              tabindex="0"
+              placeholder="Search"
+              type="text"
+              v-model="search"
+              v-on:keyup="searchCourse"
+            >
+            <i aria-hidden="true" class="material-icons icon input-group__append-icon"></i>
+          </div>
         </div>
       </v-flex>
     </v-layout>
@@ -16,16 +24,16 @@
 </template>
 
 <script>
-import Vue from 'vue';
-import EventBus from '../eventbus.js';
-import { Component } from 'vue-property-decorator';
+import Vue from "vue";
+import EventBus from "../eventbus.js";
+import { Component } from "vue-property-decorator";
 
 @Component({})
 export default class Search extends Vue {
-  search = '';
+  search = "";
 
   searchCourse() {
-    EventBus.$emit('SEARCH_COURSE', { search: this.search });
+    EventBus.$emit("SEARCH_COURSE", { search: this.search });
   }
 }
 </script>
